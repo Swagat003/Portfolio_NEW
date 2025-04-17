573d5d4c503189cecc77e17f82ed4707d9c3eff4
Swagat003 <swagat.pritam.2003@gmail.com>
2025-04-17 11:38:22 +0530

    Add Analytics

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,12 +5,14 @@ import CustomCursor from "./components/CustomCursor";
 import Footer from './components/Footer';
 import { ToastContainer } from 'react-toastify';
 import { AnimatePresence } from "motion/react";
+import { Analytics } from "@vercel/analytics/react"
 
 
 function Layout() {
   const location = useLocation();
   return (
     <>
+      <Analytics />
       <ToastContainer />
       <CustomCursor />
       <NavBar />
