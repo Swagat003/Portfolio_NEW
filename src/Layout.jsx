27106c94a0c10714dae986f8433a03bc6841a306
Swagat003 <swagat.pritam.2003@gmail.com>
2025-09-19 13:40:07 +0530

    Add Vercel Speed Insights integration

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,12 +6,14 @@ import Footer from './components/Footer';
 import { ToastContainer } from 'react-toastify';
 import { AnimatePresence } from "motion/react";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 function Layout() {
   const location = useLocation();
   return (
     <>
+      <SpeedInsights />
       <Analytics />
       <ToastContainer />
       <CustomCursor />
